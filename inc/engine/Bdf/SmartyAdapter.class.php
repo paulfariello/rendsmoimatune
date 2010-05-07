546dd176618609e93e6033197b2dd6538ea802a9
needle <needle@e4fbb011-b1e7-64bf-8665-f4bbcde577a8>
2010-05-07 12:07:31 +0000

    Correction du SmartyAdapter
    git-svn-id: file:///export/data/svn/botteDeFoin/trunk@16 e4fbb011-b1e7-64bf-8665-f4bbcde577a8

--- inc/engine/Bdf/SmartyAdapter.class.php
+++ inc/engine/Bdf/SmartyAdapter.class.php
@@ -9,27 +9,41 @@ class SmartyAdapter implements \Bdf\ITemplatesEngine {
   private $smartyInstance = null;
 
   public function setSkin($skin) {
-      if(file_exists(self::DIR.$skin) && is_dir(self::DIR.$skin)) {
+      $skinDir = ROOT.\Bdf\Core::getInstance()->getConfig("site","templates_dir").$skin.'/';
+      if(file_exists($skinDir) AND is_dir($skinDir)) {
         $this->skin = $skin;
+      }
+      if($this->smartyInstance !== null) {
+        $this->smartyInstance->template_dir = ROOT.\Bdf\Core::getInstance()->getConfig("site","templates_dir").$this->skin.'/';
       }
   }
 
   public function display($fileName) {
-    $this->smartyInstance->display($fileName.EXTENSION);
+    $this->smartyInstance->display($fileName.self::EXTENSION);
   }
 
   public function initialization() {
-    require_once(COTS."smarty/".$this->getConfig('smarty','version')."/Smarty.class.php");
+echo "init";
+    require_once(COTS."smarty/".\Bdf\Core::getInstance()->getConfig('templates','version')."/Smarty.class.php");
     $this->smartyInstance = new \Smarty();
-    $this->smartyInstance->template_dir = ROOT."templates/".$this->session->getUser()->getSkin().'/';
+    $this->smartyInstance->template_dir = ROOT.\Bdf\Core::getInstance()->getConfig("site","templates_dir").$this->skin.'/';
     $this->smartyInstance->compile_dir  = ROOT."templates_c/";
-    if($this->getConfig('logger','level') == 'Bdf::DEBUG') {
+    if(\Bdf\Core::getInstance()->getConfig('logger','level') == 'Bdf::DEBUG') {
       $this->smartyInstance->debugging = true;
     } else {
       $this->smartyInstance->debugging = false;
     }
+    $this->registerUtilsFunctions();
+  }
 
-    $this->smartyInstance->assign("bdfUtils",$this->utils);
+  private function registerUtilsFunctions() {
+   $utils = new \ReflectionClass('Bdf\Utils');
+   $methods = $utils->getMethods();
+   foreach($methods as $method) {
+     if(!$method->isConstructor() AND !$method->isDestructor() AND substr($method->name,0,2) != "__") {
+       $this->smartyInstance->register_function($method->name,array($method->class, $method->name));
+     }
+   }
   }
 
   public function assign($name, $value) {
