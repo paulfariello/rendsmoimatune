01e47a2eb6c69cfeee7838f1fc1cb2f2ec004756
Paul Fariello <paul.fariello@gmail.com>
2010-12-29 15:26:10 +0100

    Correction des problèmes de traduction

--- events/cash-up.php
+++ events/cash-up.php
@@ -53,8 +53,6 @@ $te->assign('currentEvent',$event);
 
 $messages = \Bdf\Session::getInstance()->get('messages');
 
-$messages[] = array('type'=>'info','content'=>Bdf\Utils::getText('User %1$s has been created. <a href="%2$s">Invite her/him ?</a>', "Test", "http://test"));
-
 if (null !== $messages) {
     $te->assign('messages',$messages);
     \Bdf\Session::getInstance()->remove('messages');
