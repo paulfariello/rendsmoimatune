d23eac097d4736897c14d49f85f570b33731475b
Paul Fariello <paul.fariello@gmail.com>
2012-02-05 23:33:32 +0100

    Remove unused files

--- test/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../inc/init.php';
-
-$message = "If you think toto is right please go to http://www.rendsmoimatune.eu/my-accounts/cadeaux-et-albums-18/accept-merge-11-325ed1823bb2a471ca2da73bfb4d88e4e87022659e3ab3ae68fcff443ba43238.html .";
-echo preg_replace("#([[:alpha:]]([[:alnum:]]|-)*[[:alnum:]])|([[:alpha:]])#i", '<a href="$0">$0</a>', 'a toto toto-et-tata.').'<br />';
-echo Bdf\Utils::replace_uri($message, '<a href="$0">$0</a>');
-?>
