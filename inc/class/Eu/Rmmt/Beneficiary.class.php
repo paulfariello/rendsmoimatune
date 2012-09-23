9e35d279d843fc91c20f7c745b64e4a340515ad8
Paul Fariello <paul@fariello.eu>
2012-09-23 20:05:57 +0200

    Enforce amount as integer

--- inc/class/Eu/Rmmt/Beneficiary.class.php
+++ inc/class/Eu/Rmmt/Beneficiary.class.php
@@ -49,7 +49,7 @@ class Beneficiary
     {
         $this->_expenditure = $expenditure;
         $this->_user        = $user;
-        $this->_amount      = $amount;
+        $this->_amount      = (int)$amount;
     }
 
     public function getId()
@@ -69,7 +69,7 @@ class Beneficiary
 
     public function setAmount($amount)
     {
-        $this->_amount = $amount;
+        $this->_amount = (int)$amount;
     }
 
     public function getExpenditure()
