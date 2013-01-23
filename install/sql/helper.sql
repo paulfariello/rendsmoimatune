a7e69363f2a7c1d379251f28f6a70ae808557580
Paul Fariello <paul@fariello.eu>
2013-01-23 18:47:40 +0100

    Add orphan select into sql helper

--- install/sql/helper.sql
+++ install/sql/helper.sql
@@ -1,22 +1,27 @@
 -- show all expenditures payers
-SELECT p.payer_pk, u.name, p.amount, e.title, e.amount  
-FROM account a 
-INNER JOIN expenditure e ON e.account_fk = a.account_pk 
-INNER JOIN payer p ON p.expenditure_pcfk = e.expenditure_pk 
-INNER JOIN "user" u ON u.user_pk = p.user_pcfk 
+SELECT p.payer_pk, u.name, p.amount, e.title, e.amount
+FROM account a
+INNER JOIN expenditure e ON e.account_fk = a.account_pk
+INNER JOIN payer p ON p.expenditure_pcfk = e.expenditure_pk
+INNER JOIN "user" u ON u.user_pk = p.user_pcfk
 WHERE a.account_pk = 42;
 
 -- show all expenditures beneficiaries
 SELECT b.beneficiary_pk, u.name, b.amount, e.title, e.amount
-FROM account a 
-INNER JOIN expenditure e ON e.account_fk = a.account_pk 
-INNER JOIN beneficiary b ON b.expenditure_pcfk = e.expenditure_pk 
-INNER JOIN "user" u ON u.user_pk = b.user_pcfk 
+FROM account a
+INNER JOIN expenditure e ON e.account_fk = a.account_pk
+INNER JOIN beneficiary b ON b.expenditure_pcfk = e.expenditure_pk
+INNER JOIN "user" u ON u.user_pk = b.user_pcfk
 WHERE a.account_pk = 42;
 
 -- show all repayment
-SELECT r.repayment_pk, p.name, r.amount, b.name FROM account a 
-INNER JOIN repayment r ON r.account_fk = a.account_pk 
-INNER JOIN "user" p ON p.user_pk = r.payer_fk 
-INNER JOIN "user" b ON b.user_pk = r.beneficiary_fk 
+SELECT r.repayment_pk, p.name, r.amount, b.name FROM account a
+INNER JOIN repayment r ON r.account_fk = a.account_pk
+INNER JOIN "user" p ON p.user_pk = r.payer_fk
+INNER JOIN "user" b ON b.user_pk = r.beneficiary_fk
 WHERE a.account_pk = 42;
+
+-- search for orphan users
+SELECT * FROM "user" u
+LEFT OUTER JOIN account_user a ON u.user_pk = a.user_pcfk
+WHERE u.registered = false AND a.account_pcfk IS NULL;
