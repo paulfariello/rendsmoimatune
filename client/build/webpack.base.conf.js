3abafba1f28bee1a2f18bf790be2f52ece803767
Paul Fariello <pfariello@arkoon.dev>
2016-04-28 18:38:02 +0200

    Install vue with foundation

---
+++ client/build/webpack.base.conf.js
@@ -0,0 +1,92 @@
+var path = require('path')
+var config = require('../config')
+var utils = require('./utils')
+var autoprefixer = require('autoprefixer')
+var projectRoot = path.resolve(__dirname, '../')
+var nodeModules = path.join(projectRoot, 'node_modules')
+
+module.exports = {
+	entry: {
+		app: './src/main.js'
+	},
+	output: {
+		path: config.build.assetsRoot,
+		publicPath: config.build.assetsPublicPath,
+		filename: '[name].js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.vue'],
+		fallback: [nodeModules],
+		alias: {
+			'src': path.resolve(__dirname, '../src'),
+			'assets': path.resolve(__dirname, '../src/assets'),
+			'components': path.resolve(__dirname, '../src/components')
+		}
+	},
+	resolveLoader: {
+		fallback: [nodeModules]
+	},
+	module: {
+		preLoaders: [
+		{
+			test: /\.vue$/,
+			loader: 'eslint',
+			include: projectRoot,
+			exclude: /node_modules/
+		},
+		{
+			test: /\.js$/,
+			loader: 'eslint',
+			include: projectRoot,
+			exclude: /node_modules/
+		}
+		],
+		loaders: [
+		{
+			test: /\.vue$/,
+			loader: 'vue'
+		},
+		{
+			test: /\.js$/,
+			loader: 'babel',
+			include: projectRoot,
+			exclude: /node_modules/
+		},
+		{
+			test: /\.json$/,
+			loader: 'json'
+		},
+		{
+			test: /\.html$/,
+			loader: 'vue-html'
+		},
+		{
+			test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+			loader: 'url',
+			query: {
+				limit: 10000,
+				name: utils.assetsPath('img/[name].[hash:7].[ext]')
+			}
+		},
+		{
+			test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+			loader: 'url',
+			query: {
+				limit: 10000,
+				name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+			}
+		},
+		]
+	},
+	postcss: function(webpack) {
+		return [
+			autoprefixer({browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']})
+		]
+	},
+	sassLoader: {
+		includePaths: [nodeModules]
+	},
+	eslint: {
+		formatter: require('eslint-friendly-formatter')
+	},
+}
---
+++ client/src/main.js
@@ -0,0 +1,9 @@
+import 'src/css/main.scss'
+import Vue from 'vue'
+import App from './App'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App }
+})
