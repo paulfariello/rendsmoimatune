c2d48462be670e325c0f64821b977ee09fc26443
Paul Fariello <pfariello@arkoon.dev>
2016-06-03 13:56:15 +0200

    Remove useless imports for foundation-datepicker

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 import 'src/css/main.scss'
 import $ from 'jquery'
-import 'imports?this=>window!foundation-datepicker/js/foundation-datepicker.js'
+import 'foundation-datepicker/js/foundation-datepicker.js'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
